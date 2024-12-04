3190f8167000c6539278821d5e668ae845274d11
VASQUEZ VARGAS EDWIN <edwin.vasquez0417@gmail.com>
2024-12-04 12:44:10 -0500

    v1.0.1

--- app/src/main/java/com/codevex/compose/codevexcore/MainActivity.kt
+++ app/src/main/java/com/codevex/compose/codevexcore/MainActivity.kt
@@ -10,8 +10,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.codevex.compose.codevexcore.ui.theme.CodevexCoreTheme
+import com.codevex.compose.mcore.extensions.randomPastel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,7 +36,8 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
+        color = Color.randomPastel()
     )
 }
 
